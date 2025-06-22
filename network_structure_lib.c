1e020b2ad3c43e0dacf5adcb3d0687302b665677
Mauro <mauro.presso@gmail.com>
2025-06-22 17:38:27 -0300

    .

---
+++ network_structure_lib.c
@@ -0,0 +1,11 @@
+/**
+ * @file network_structure_lib.h
+ * @brief Desarrollos de funciones para manipulación de datos.
+ *
+ * Este archivo contiene los desarrollos de funciones para manipular datos.
+*/
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
