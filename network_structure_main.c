72bf3163e3dbf849eb1d0d92331204976230465d
Mauro <mauro.presso@gmail.com>
2025-06-22 17:41:57 -0300

    Incluyendo libreías

---
+++ network_structure_main.c
@@ -0,0 +1,12 @@
+/**
+ * @file network_structure_lib.h
+ * @brief Desarrollos de funciones para manipulación de datos.
+ *
+ * Este archivo contiene los desarrollos de funciones para manipular datos.
+*/
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "network_structure_lib.h"
