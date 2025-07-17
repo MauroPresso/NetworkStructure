3078f94b74ce0a765d7497f517b3ff84a0c03437
Mauro <mauro.presso@gmail.com>
2025-07-17 18:04:29 -0300

    Este es el programa donde se realizará el inciso nro 2 del examen.

---
+++ network_structure_prog.c
@@ -0,0 +1,16 @@
+/**
+ * @file network_structure_main.c
+ * @brief Programa donde se pone a prueba el funcionamiento de las funciones del inciso nro 1.
+ *
+ * Este archivo contiene el programa donde se pone a prueba el funcionamiento de las funciones del inciso nro 1.
+*/
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "network_structure_lib.h"
+
+int prog()
+{
+    return 0;
+}
